60638a2d1a684f23a301ff52e9c8f6156774c2b9
soohyunnn <sksmsqodn20@gmail.com>
2020-11-22 16:18:46 +0900

    useReducer로 상태 관리하기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,68 @@
-import React, { useState, useRef } from 'react';
+import React, {useRef, useEffect, useReducer, useCallback } from 'react';
+import axios from "axios";
 import './App.css';
+
+
+
+
+const initialState = {
+  input : '',
+  todos: []
+}
+
+function reducer(state, action) {
+  switch(action.type){
+    case "CHANGE_INPUT":
+      return {
+        ...state,
+        input : action.value,
+      };
+    case "RESET_INPUT":
+      //console.log(state.input);
+      return {
+        ...state,
+        input: ''
+      };
+    case "CREATE_TODO":
+      //console.log(state.todos);
+      return {
+        input: initialState.input,
+        todos: state.todos.concat(action.todo),
+      };
+    case "DELETE_TODO":
+      return {
+        ...state,
+        todos : state.todos.filter(todo => todo.id !== action.id)
+      };
+    case "TOGGLE_TODO":
+      return {
+        ...state,
+        todos : state.todos.map(todo => 
+          todo.id === action.id ? {...todo, action : !todo.action} : todo
+        )
+      }
+    default:
+      return state;
+  }
+}
+
+async function getTodos() {
+  const response = await axios.get(
+    "/todos"
+  );
+  console.log(response.data);
+  return response.data;
+}
 
 function App() {
 
-  const [input, setInput] = useState('');
-
-  const [todos, setTodos] = useState([]);
-
-
+  const [state, dispatch] = useReducer(reducer, initialState);
   const id = useRef(1);
+  const {todos} = state;
+
+  useEffect(()=> {
+    getTodos();
+  },[])
 
   const onEnterKeyPress = (e) => {
     if (e.key === 'Enter'){
@@ -16,59 +70,63 @@ function App() {
     }
   }
 
-  const onChange = (e) => {
-    setInput(e.target.value);
-  };
+  const onChange = useCallback(e => {
+    const {value} = e.target;
+    dispatch({
+      type : "CHANGE_INPUT",
+      value
+    });
+  }, []);
 
-  const onCreate = () => {
-    const todo  = {
-      id : id.current,
-      text : input,
-      action: true,
-    }
-    setTodos([
-      ...todos,
-      todo
-    ])
+  const onCreate = useCallback(() => {
+    dispatch({
+      type : "CREATE_TODO",
+      todo : {
+        id: id.current,
+        text: state.input,
+        action: true,
+      }
+    });
+    id.current += 1;
+  },[state.input]);
 
-    setInput('')
-    id.current++;
+  const onRemove = useCallback(() => {
+   dispatch({
+     type: "RESET_INPUT"
+   });
+  },[])
 
-  }
+  const onDelete = useCallback((id) => {
+   dispatch({
+     type: "DELETE_TODO",
+     id,
+   })
+  },[todos]);
 
-  const onRemove = () => {
-    setInput('')
-  }
-
-  const onDelete = (id) => {
-    setTodos(todos.filter(todo => todo.id !== id))
-  }
-
-  const onToggle = (action, id) => {
-    console.log('action',action);
+  const onToggle = useCallback((id) => {
     console.log('id',id);
-    setTodos(
-      todos.map(todo => todo.id === id ? {...todo, action : !todo.action }: todo)
-    )
-  }
+    dispatch({
+      type : "TOGGLE_TODO",
+      id
+    })
+  },[todos]);
 
   return (
     <>
     <div className="App">
       <h1>Todos</h1>
-      <input onChange={onChange} onKeyPress={onEnterKeyPress} value={input} />   
+      <input onChange={onChange} onKeyPress={onEnterKeyPress} value={state.input} />   
       <button className="button" onClick={onCreate}>등록</button>
       <button className="button" onClick={onRemove}>초기화</button>
       <h2>♥ List ♥</h2>
       <div>
-        
           {todos.map(todo => 
             <>
             <div className="todolist">
               <li
-               style={{color : todo.action === true ? 'green' : 'purple'}} 
+                style={{color : todo.action === true ? 'green' : 'purple'}} 
                key={todo.id} 
-               onClick={() => onToggle(todo.action, todo.id)}
+               onClick={() => onToggle(todo.id)}
                >
                  {todo.text}
               <button
