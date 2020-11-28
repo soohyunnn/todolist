84a3d256961055b0a01c0c47cfe587a802a4f8fe
soohyunnn <sksmsqodn20@gmail.com>
2020-11-28 17:23:01 +0900

    Date함수로 날짜 불러오기 수정

--- src/TodoHead.js
+++ src/TodoHead.js
@@ -2,11 +2,21 @@ import React from 'react';
 import './App.css';
 
 function TodoHead(){
+    //날짜 satrt
+    const today = new Date();
+    const dateString =  today.toLocaleDateString('ko-KR', {
+       year: 'numeric',
+       month: 'long',
+       day: 'numeric' 
+    });
+    const dayName = today.toLocaleDateString('ko-KR', {weekday: 'long'});
+    //날짜 end
+
     return(
         <div className="TodoHeadBlock">
             <h1>Todos</h1>
-            <h1>2020년 11월 13일</h1>
-            <div className="day">금요일</div>
+            <h1>{dateString}</h1>
+            <div className="day">{dayName}</div>
         </div>
     )
 }
